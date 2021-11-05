16ae01b74b8fcfc7b89f8d52b4ebf544b2adfea9
Premdas Golhar <prem.golhar@gmail.com>
2021-11-05 14:10:56 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "network" {
   source       = "app.terraform.io/prem-db/network/google"
   version      = "3.4.0"
   network_name = "gaurav-network"
-  project_id   = var.project_id
+  project_id   = var.project
   subnets = [
     {
       subnet_name   = "gaurav-subnet"
